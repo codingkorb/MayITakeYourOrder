b361359e6ad3d197b1d120b76ca8cc6be9e7aae8
harrykorb <codingkorb@gmail.com>
2022-12-04 17:20:24 +0100

    Added Manual shop test in Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,21 @@
 package org.example;
 
+import model.Product;
+import repo.ProductRepo;
+import service.ShopService;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Product> products = List.of(
+                new Product("F004", "Mousaka"),
+                new Product("F005", "Baklava"),
+                new Product("F006", "Briam")
+        );
+        ProductRepo productRepo = new ProductRepo(products);
+        ShopService shopService = new ShopService(productRepo);
+        System.out.println(shopService.listProducts());
+        System.out.println(shopService.getProduct("F004"));
     }
 }
